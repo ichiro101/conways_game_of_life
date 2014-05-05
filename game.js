ff4dce6c61355ea1a1cd3a05d8bd312a3283e920
Ichiro Yamamoto <yichi@otakukingdom.com>
2014-05-05 13:47:35 -0600

    board initialization

--- game.js
+++ game.js
@@ -1,10 +1,30 @@
+var Board = function() {
+  // store the board state in this two dimensional array
+  this.boardArray = new Array();
+
+  // size of the board
+  nVerticalElem = 80;
+  nHorizontalElem = 80;
+
+  // create the two dimensional array here
+  // initialize everything to 0
+  for(i = 0; i < nVerticalElem; i++) {
+    this.boardArray[i] = new Array();
+    for(j = 0; j < nHorizontalElem; j++) {
+      this.boardArray[i][j] = 0;
+    }
+  }
+}
+
 function sketchProc(ps) {
 
   // Override draw function, by default it will be called 60 times per second
   ps.draw = function() {
+    // set up the background and size
     ps.background(224);
     ps.size(800, 800);
 
+    // now draw the board
   };
 }
 
--- tests.js
+++ tests.js
@@ -1,3 +1,9 @@
 test("check qunit is in working order", function() {
   ok(1 === 1, "qunit is working");
 });
+
+test("basic board functionality", function() {
+  var board = new Board();
+  ok(board.boardArray.length == 80, "correct verticle size");
+  ok(board.boardArray[0].length == 80, "correct horizontal size");
+});
